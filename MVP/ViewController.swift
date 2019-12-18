531401fa2bcaa9ea48037e14ae65e3e64a82099f
Francis Bergonia <francisb.itcareers@gmail.com>
2019-12-18 22:10:02 +0800

    Initial Commit

---
+++ MVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by Francis Jemuel Bergonia on 12/18/19.
+//  Copyright © 2019 Xi Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
